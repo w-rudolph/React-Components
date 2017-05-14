a9e4f5c78e1b971ed53c1fabcc0173ed7b2753c5
e <172247097@qq.com>
2017-05-14 12:23:58 +0800

    fix

--- src/entry.js
+++ src/entry.js
@@ -9,15 +9,15 @@ class Home extends React.Component{
     constructor(props) {
         super(props);
     }
-    onInut(v){
+    onInput(v){
         console.log(v); 
     }
     render(){
         return (
             <div className="page-container">
-                <p><Input onInut={this.onInut} width="200px" type="text" /> </p>
+                <p><Input onInput={this.onInput} width="200px" type="text" /> </p>
                 
-                <p><TextArea noResize onInut={this.onInut}  width="200px" rows={3} placeholder="input here ..."/></p>
+                <p><TextArea noResize onInput={this.onInput}  width="200px" rows={3} placeholder="input here ..."/></p>
             </div>
         )
     }
